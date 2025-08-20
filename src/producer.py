0f98b8849642fe5f23e0693741b81c8fa971ddf5
GarettKaube <gkaube@outlook.com>
2025-08-20 16:06:01 -0600

    Added more documentation

--- src/producer.py
+++ src/producer.py
@@ -14,6 +14,7 @@ import datetime
 import json
 from pandas import IndexSlice as idx
 from src.utils import get_param
+from typing import Tuple
 
 import os
 
@@ -40,41 +41,51 @@ AWS_REGION = os.environ.get("REGION")
 kinesis_client = boto3.client("kinesis", region_name=AWS_REGION)
 
 
-def generate_time_stamp_range(current_timestamp, periods:int) -> np.ndarray:
+def generate_time_stamp_range(current_timestamp: pd.Timestamp, periods:int) -> np.ndarray:
+    """
+    Given a pickup time stamp from the rides data, a numpy array of random dates is
+    generated ranging from 15 minutes prior of current_timestamp to current_timestamp
+    :param current_timestamp: pd.Timestamp to generate dates between [current_timestamp - 15mins, current_timestamp]
+    :param periods:
+    :return: np.ndarray np.datetime64
+    """
     start = current_timestamp - pd.Timedelta(minutes=15)
     end = current_timestamp
     return pd.date_range(start=start, end=end, periods=periods).to_numpy()
 
 
-def assign_df_random_time_stamp(df:pd.DataFrame, time_stamps:np.ndarray, periods:int) -> list[float]:
-    # Use uniform dist to select random indices for time stamps for pickup times
-    random_u = np.random.randint(low=0, high=periods, size=df.shape[0])
+def assign_df_random_time_stamp(df:pd.DataFrame, timestamps:np.ndarray) -> list:
+    """
+    :param df: Pandas DataFrame to assign timestamps
+    :param timestamps: np.array of timestamps
+    :return: list of randomized timestamps
+    """
+    # Use uniform dist to select random indices for timestamps for pickup times
+    random_u = np.random.randint(low=0, high=timestamps.shape[0], size=df.shape[0])
     if random_u.shape[0] != df.shape[0]:
         raise ValueError(f"random_u.shape[0] does not equal df_at_timestamp.shape[0]")
 
-    return  time_stamps[random_u].tolist()
+    return  timestamps[random_u].tolist()
 
 
-def shuffle_df(df:pd.DataFrame) -> pd.DataFrame:
+def shuffle_df(df: pd.DataFrame) -> pd.DataFrame:
     """
     Randomly shuffles Pandas DataFrame
-    :param df:
-    :return:
+    :param df: pd.DataFrame
+    :return: shuffled pd.DataFrame
     """
     return df.sample(frac=1)
 
 
-def generate_random_timestamps(df: pd.DataFrame) -> pd.DataFrame:
+def generate_random_timestamps(df: pd.DataFrame, periods=1000) -> pd.DataFrame:
     """
     Addresses that the pickup times are rounded to nearest 15 minute by
     constructing a random timestamp for each observation that is within the range of the
     observations pickup time stamp and the past 15 minutes of the pickup.
     :param df: pandas dataframe with datetime index
+    :param periods number of possible time stamps to be selected when random sampling
     :return: pd.DataFrame
     """
-    # number of possible time stamps to be selected when random sampling
-    periods = 1000
-
     unique_timestamps = df.index.unique()
 
     all_timestamps = []
@@ -89,7 +100,7 @@ def generate_random_timestamps(df: pd.DataFrame) -> pd.DataFrame:
         time_stamps = generate_time_stamp_range(time_stamp, periods)
 
         # Sample time stamps
-        time_stamps = assign_df_random_time_stamp(df_at_timestamp, time_stamps, periods)
+        time_stamps = assign_df_random_time_stamp(df_at_timestamp, time_stamps)
 
         all_timestamps.extend(time_stamps)
 
@@ -97,7 +108,12 @@ def generate_random_timestamps(df: pd.DataFrame) -> pd.DataFrame:
     return  df
 
 
-def get_time_deltas_seconds(time_stamps):
+def get_time_deltas_seconds(time_stamps: pd.DataFrame) -> list:
+    """
+    Differences the input time_stamps and converts the differences to seconds
+    :param time_stamps:
+    :return: list of deltas in seconds
+    """
     time_deltas = [np.nan]
     for i in range(1, time_stamps.shape[0]):
         delta = (time_stamps[i] - time_stamps[i-1]).total_seconds()
@@ -106,12 +122,13 @@ def get_time_deltas_seconds(time_stamps):
     return time_deltas
 
 
-def shift_aggregates(df:pd.DataFrame, length):
+def shift_aggregates(df:pd.DataFrame, length: int) -> Tuple[pd.DataFrame, pd.Timestamp]:
     """
-    expects df with multiindex (community, time_stamps)
-    :param df:
-    :param length:
-    :return:
+    To implement rolling aggregates, this function removes to most historic timestamp for each community
+    if there is more than length timestamps present.
+    :param df: expects df with multiindex (community, time_stamps)
+    :param length: int of maximum number of timestamps that each community can have
+    :return: Tuple[pd.DataFrame, pd.Timestamp] shifted dataframe and the timestamp that was removed
     """
     removed_ts = None
 
@@ -122,7 +139,13 @@ def shift_aggregates(df:pd.DataFrame, length):
     return  df, removed_ts
 
 
-def aggregate_data(df:pd.DataFrame):
+def aggregate_data(df:pd.DataFrame) -> pd.DataFrame:
+    """
+    Groups by df by pickup_community_area and 30 minute time intervals and calculates statistics for each group.
+    The function ensures each possible 30 minute time interval timestamp exists for each community.
+    :param df: df to aggregate
+    :return: aggregated df
+    """
     aggregate =  df[[
             "pickup_community_area",
             "random_time_stamp",
@@ -149,7 +172,13 @@ def aggregate_data(df:pd.DataFrame):
     return aggregate
 
 
-def update_current_time(current_time, item):
+def update_current_time(current_time: pd.Timestamp, item: pd.Series) -> pd.Timestamp:
+    """
+    compares current time and item['trip_start_timestamp'] to update current_time
+    :param current_time: pd.Timestamp to be updated
+    :param item: pd.Series containing trip_start_timestamp datetime column
+    :return:
+    """
     if current_time is None:
         current_time = item['trip_start_timestamp']
     elif current_time - item['trip_start_timestamp'] != datetime.timedelta(0):
@@ -158,6 +187,12 @@ def update_current_time(current_time, item):
 
 
 def simulate_data(df: pd.DataFrame, stream: str, stream_speed_factor: int | float) -> None:
+    """
+    :param df: rides data to loop over to simulate
+    :param stream: AWS Kinesis stream name
+    :param stream_speed_factor: speeds up or slows down the simulation
+    :return: None
+    """
     # Buffer for observed data
     data = pd.DataFrame()
 
